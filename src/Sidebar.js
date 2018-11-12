b220bd82b4bd2e1e3f0aa81957084f54435aee13
colmlg <colm.legear@gmail.com>
2018-11-12 13:16:28 +0000

    Initial app setupˆ

---
+++ src/Sidebar.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import {
+    AppSidebar,
+    AppSidebarFooter,
+    AppSidebarForm,
+    AppSidebarHeader,
+    AppSidebarMinimizer,
+    AppSidebarNav
+} from "@coreui/react";
+
+
+let navigation ={ items: [
+    {
+        name: 'Dashboard',
+        url: '/dashboard',
+        icon: 'icon-speedometer',
+        // badge: {
+        //   variant: 'info',
+        //   text: 'NEW',
+        // },
+    },
+    {
+        title: true,
+        name: 'Modules',
+        wrapper: {            // optional wrapper object
+            element: '',        // required valid HTML5 element tag
+            attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+        },
+        class: ''             // optional class names space delimited list for title item ex: "text-center"
+    }
+]}
+
+class Sidebar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            navigation: navigation
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3000/module").then(results => {
+            return results.json();
+        }).then(modules => {
+            modules.forEach(module => {
+                navigation.items.push({
+                    name: module.id.toUpperCase(),
+                    url: '/module/' + module.id,
+                    icon: 'icon-book-open',
+                })
+            });
+
+            this.setState({ navigation: navigation });
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    render() {
+        return(
+            <AppSidebar fixed display="lg">
+                <AppSidebarHeader />
+                <AppSidebarForm />
+                <AppSidebarNav navConfig={navigation} />
+                <AppSidebarFooter />
+                <AppSidebarMinimizer />
+            </AppSidebar>
+        )
+    }
+}
+
+export default Sidebar;
+
