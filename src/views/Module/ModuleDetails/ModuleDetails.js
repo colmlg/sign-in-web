3b150a408c17ca5aadab88d02a2cdd963bcc224c
Colm Le Gear <colm.legear@gmail.com>
2019-03-21 18:36:01 +0000

    Attedance statistics

--- src/views/Module/ModuleDetails/ModuleDetails.js
+++ src/views/Module/ModuleDetails/ModuleDetails.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {
-    Card, CardBody,
-    CardHeader, Table
-} from 'reactstrap';
+import {Card, CardBody, CardHeader, Col, Row, Table} from 'reactstrap';
+import CircularProgressbar from 'react-circular-progressbar';
 import dateFormat from 'dateformat';
 import ModuleService from '../../../services/ModuleService';
+import 'react-circular-progressbar/dist/styles.css';
+import './progress.css';
 
 class ModuleDetails extends Component {
 
@@ -15,14 +15,16 @@ class ModuleDetails extends Component {
 
         this.state = {
             lessons: [],
-            module: {
-                students: [],
+            students: [],
+            attendance: {
+                student: [],
+                overall: 0,
             },
-            moduleId: props.match.params.id
+            moduleId: props.match.params.id,
         };
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.getModules(this.state.moduleId);
     }
 
@@ -34,10 +36,13 @@ class ModuleDetails extends Component {
         ModuleService.getModule(id).then(module => {
             const today = new Date();
             module.lessons = module.lessons.filter(lesson => new Date(lesson.date) <= today);
+            const attendance = this.calculateOverallAttendance(module);
+
             this.setState({
                 moduleId: id,
-                module: module.module,
+                students: module.module.students,
                 lessons: module.lessons,
+                attendance: attendance,
             });
         }).catch(error => {
             alert(error);
@@ -51,36 +56,63 @@ class ModuleDetails extends Component {
     }
 
     createTableRows() {
-        return this.state.module.students.map(student => {
+        const rows = this.state.students.map(student => {
             return <tr>
                 <td>{student}</td>
-                {this.calculateAttendance(student)}
-                {this.getModuleAttendance(student)}
+                {this.getOverallAttendance(student)}
+                {this.getLessonAttendance(student)}
             </tr>
-        })
+        });
+
+        return rows;
     }
 
-    getModuleAttendance(student) {
+    getLessonAttendance(student) {
         return this.state.lessons.map(lesson => {
             return <td>{lesson.studentsAttended.indexOf(student) === -1 ? "" : "X"}</td>
         })
     }
 
-    calculateAttendance(student) {
-        const totalLessons = this.state.lessons.length;
-        const lessonsAttended = this.state.lessons.filter(lesson => lesson.studentsAttended.indexOf(student) !== -1).length;
-        const percentage = ((lessonsAttended / totalLessons) * 100.0).toFixed(0);
+    getOverallAttendance(student) {
+
+        const percentage = this.state.attendance.student.find(e => e.id === student).attendance.toFixed(0);
 
         let color;
-        if(percentage >= 75) {
+        if (percentage >= 75) {
             color = "table-success";
-        } else if(percentage >= 50) {
+        } else if (percentage >= 50) {
             color = "table-warning";
         } else {
             color = "table-danger";
         }
 
+
         return <td class={color}>{percentage + "%"}</td>;
+    }
+
+    calculateOverallAttendance(module) {
+        let attendance = {
+            student: [],
+            overall: 0,
+        };
+
+        const totalLessons = module.lessons.length;
+
+        module.module.students.forEach(s => {
+            const lessonsAttended = module.lessons.filter(l => l.studentsAttended.indexOf(s) !== -1).length;
+            const percentage = ((lessonsAttended / totalLessons) * 100.0); //.toFixed(0)
+            attendance.student.push({id: s, attendance: percentage });
+        });
+
+        //Calculate average attendance
+        let sum = 0;
+        attendance.student.forEach(s => {
+            sum += parseInt(s.attendance);
+        });
+
+        attendance.overall = sum / attendance.student.length;
+
+        return attendance;
     }
 
     render() {
@@ -90,6 +122,42 @@ class ModuleDetails extends Component {
                     <strong>{this.state.moduleId}</strong>
                 </CardHeader>
                 <CardBody>
+                    <Row>
+                        <Col>
+                            <CircularProgressbar className={"module-details"}
+                                                 percentage={this.state.attendance.overall}
+                                                 text={`${this.state.attendance.overall}%`}/>
+                        </Col>
+                        <Col>
+                            <CircularProgressbar className={"module-details"}
+                                                 percentage={78}
+                                                 text={`${98}%`}/>
+                        </Col>
+                        <Col>
+                            <CircularProgressbar className={"module-details"}
+                                                 percentage={45}
+                                                 text={`${45}%`}/>
+                        </Col>
+                        <Col>
+                            <CircularProgressbar className={"module-details"}
+                                                 percentage={90}
+                                                 text={`${90}%`}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h5 style={{textAlign: 'center',}}>Overall Attedance</h5>
+                        </Col>
+                        <Col>
+                            <h5 style={{textAlign: 'center',}}>Lectures</h5>
+                        </Col>
+                        <Col>
+                            <h5 style={{textAlign: 'center',}}>Labs</h5>
+                        </Col>
+                        <Col>
+                            <h5 style={{textAlign: 'center',}}>Tutorials</h5>
+                        </Col>
+                    </Row>
                     <Table responsive striped hover bordered size="sm">
                         <thead>
                         <tr>
@@ -99,7 +167,7 @@ class ModuleDetails extends Component {
                         </tr>
                         </thead>
                         <tbody>
-                            {this.createTableRows()}
+                        {this.createTableRows()}
                         </tbody>
                     </Table>
                 </CardBody>
